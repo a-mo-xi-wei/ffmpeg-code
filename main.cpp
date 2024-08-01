4826ca34810b9a70ce0e27b02979d8c43cb17497
weisang <1428206861@qq.com>
2024-08-01 21:50:14 +0800

    图像格式转换

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@ extern "C" {
 #include"libavformat/avio.h"
 #include"libavutil/file.h"
 #include"libswresample/swresample.h"
+#include"libswscale/swscale.h"
+#include"libavutil/imgutils.h"
 }
 
 using namespace std;
@@ -17,9 +19,9 @@ using namespace std;
 int main() {
 
 	string inputFile = __FILE__;
-	inputFile += "/../Res/out.aac";
+	inputFile += "/../Res/video.mp4";
 	string outputFile = __FILE__;
-	outputFile += "/../Res/out1.pcm";
+	outputFile += "/../Res/out.rgb";
 
 	AVFormatContext* fmtCtx = nullptr;
 	int ret = avformat_open_input(&fmtCtx, inputFile.c_str(), nullptr, nullptr);
@@ -27,102 +29,88 @@ int main() {
 		cerr << "打开失败" << endl;
 		return -1;
 	}
-	
+
 	avformat_find_stream_info(fmtCtx, nullptr);
 
-	int index = av_find_best_stream(fmtCtx, AVMEDIA_TYPE_AUDIO, 
-		-1, -1, nullptr,0);
-	if (index < 0) {
+	int index = av_find_best_stream(fmtCtx, AVMEDIA_TYPE_VIDEO,
+		-1, -1, nullptr, 0);
+	if (ret < 0) {
 		cerr << "查找失败" << endl;
 		return -1;
 	}
 
-	const AVCodec* codec = avcodec_find_decoder(fmtCtx->streams[index]->codecpar->codec_id);
+	const AVCodec* codec = avcodec_find_decoder(
+		fmtCtx->streams[index]->codecpar->codec_id);
+
 	AVCodecContext* codecCtx = avcodec_alloc_context3(codec);
+	if (!codecCtx) {
+		cerr << "分配失败" << endl;
+		return -1;
+	}
 	avcodec_parameters_to_context(codecCtx, fmtCtx->streams[index]->codecpar);
 	ret = avcodec_open2(codecCtx, codec, nullptr);
-	if (ret != 0) {
+	if (ret < 0) {
 		cerr << "打开失败" << endl;
 		return -1;
 	}
-
-	SwrContext* swrCtx = nullptr;
-	ret = swr_alloc_set_opts2(&swrCtx,
-		&codecCtx->ch_layout,
-		AV_SAMPLE_FMT_S16,
-		48000,
-		&codecCtx->ch_layout,
-		codecCtx->sample_fmt,
-		codecCtx->sample_rate,
-		0,nullptr);
-	if (ret != 0) {
-		cerr << "创建失败" << endl;
+	SwsContext* swsCtx = sws_getContext(codecCtx->width, codecCtx->height,
+		codecCtx->pix_fmt, 640, 480, AV_PIX_FMT_RGB24, SWS_BICUBIC, nullptr, nullptr, nullptr);
+	if (!swsCtx) {
 		return -1;
 	}
 
-	ret = swr_init(swrCtx);
-	if (ret < 0)return -1;
+	uint8_t* buffer[4];
+	int lineSize[4];
+	int bufferSize;
 
-	ofstream outFile(outputFile, ostream::binary);
+	bufferSize = av_image_alloc(buffer, lineSize, 640, 480, AV_PIX_FMT_RGB24, 1);
+	if (bufferSize < 0)return -1;
+	ofstream outFile(outputFile, ofstream::binary);
 	if (!outFile.is_open()) {
+		cerr << "打开失败" << endl;
 		return -1;
 	}
 	AVPacket* pkt = av_packet_alloc();
 	AVFrame* frame = av_frame_alloc();
+	if (!pkt || !frame) {
+		cerr << "分配失败" << endl;
+		return -1;
+	}
 
-	uint8_t** buffer = nullptr;
-	int bufferSize = 0;
-	int64_t out_sample = av_rescale_rnd(
-		swr_get_delay(swrCtx,codecCtx->sample_rate) + codecCtx->frame_size,
-		48000,
-		codecCtx->sample_rate,
-		AV_ROUND_UP);
+	while (av_read_frame(fmtCtx, pkt) == 0){
+		if (pkt->stream_index != index)continue;
 
-	av_samples_alloc_array_and_samples(&buffer, &bufferSize,2,out_sample,
-		AV_SAMPLE_FMT_S16,0);
-	while (av_read_frame(fmtCtx, pkt) == 0) {
 		ret = avcodec_send_packet(codecCtx, pkt);
-		if (ret != 0) {
-			return -1;
-		}
-		while (ret == 0) {
+		if (ret < 0)break;
+
+		while (ret >= 0) {
 			ret = avcodec_receive_frame(codecCtx, frame);
-			if (ret < 0)break;
-			
-			swr_convert(swrCtx, buffer, out_sample, frame->data, 
-				frame->nb_samples);
+			if (ret != 0)break;
 
-			int useBufferSize = av_samples_get_buffer_size(&bufferSize, 2, 
-				out_sample, AV_SAMPLE_FMT_S16, 1);
-			if (useBufferSize < 0)return -1;
-			outFile.write((char*)buffer[0], useBufferSize);
-
+			sws_scale(swsCtx, frame->data, frame->linesize, 0, frame->height,
+				buffer, lineSize);
+			outFile.write((char*)buffer[0], bufferSize);
 			av_frame_unref(frame);
 		}
 		av_packet_unref(pkt);
 	}
 	ret = avcodec_send_packet(codecCtx, nullptr);
-	while (ret == 0) {
+	while (ret >= 0) {
 		ret = avcodec_receive_frame(codecCtx, frame);
-		if (ret < 0)break;
+		if (ret != 0)break;
 
-		swr_convert(swrCtx, buffer, out_sample, frame->data,
-			frame->nb_samples);
-
-		int useBufferSize = av_samples_get_buffer_size(&bufferSize, 2,
-			out_sample, AV_SAMPLE_FMT_S16, 1);
-		if (useBufferSize < 0)return -1;
-		outFile.write((char*)buffer[0], useBufferSize);
-
+		sws_scale(swsCtx, frame->data, frame->linesize, 0, frame->height,
+			buffer, lineSize);
+		outFile.write((char*)buffer[0], bufferSize);
 		av_frame_unref(frame);
 	}
 	av_packet_unref(pkt);
 
-	outFile.close();
 	av_packet_free(&pkt);
 	av_frame_free(&frame);
 	avformat_close_input(&fmtCtx);
 	avcodec_free_context(&codecCtx);
-	swr_free(&swrCtx);
+
+
 	return 0;
 }
